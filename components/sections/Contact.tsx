177ae23ff0c219c8ea70c1af018eff017c1cde20
hampusvh <hampus.vh@gmail.com>
2025-12-26 12:25:04 +0100

    contact placeholder section

--- components/sections/Contact.tsx
+++ components/sections/Contact.tsx
@@ -1,6 +1,20 @@
+import Section from "@/components/layout/Section";
+import Container from "@/components/layout/Container";
+import "@/styles/sections/contact.css";
+
 export default function Contact() {
     return (
-        <section>
-        </section>
+        <Section>
+            <Container>
+                <div className="contact-cta">
+                    <p>Get in touch</p>
+
+                    <div className="contact-actions">
+                        <a href="#">Email</a>
+                        <a href="#">LinkedIn</a>
+                    </div>
+                </div>
+            </Container>
+        </Section>
     );
 }
