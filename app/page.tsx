335fb56a9451344dcfabf7d15a79b9edba06b5a6
hampusvh <hampus.vh@gmail.com>
2025-12-30 17:25:54 +0100

    remove about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Hero from "@/components/sections/Hero";
 import Projects from "@/components/sections/Projects";
 import Tech from "@/components/sections/Tech";
-import About from "@/components/sections/About";
 import Contact from "@/components/sections/Contact";
 
 export default function Page() {
